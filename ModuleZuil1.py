c2061ddf0695d193fad754911ec99124a88129be
Morid Aziz <ahmad.morid.aziz@hva.nl>
2023-09-23 00:02:59 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	ModuleZuil1.py

--- ModuleZuil1.py
+++ ModuleZuil1.py
@@ -1,19 +1,25 @@
 import csv
+# https://docs.python.org/3.10/library/csv.html
 import datetime
+# https://www.w3schools.com/python/python_datetime.asp
 import random
+# https://docs.python.org/3/library/random.html
 
-stations = ["Station A", "Station B", "Station C"]
+stations = ["Amsterdam", "Utrecht", "Leiden", "Groningen"]
 def enter_message():
-    message = input("Enter your message (up to 140 characters): ")
+    message = input("Write your message (no more than 140 characters): ")
     if len(message) > 140:
-        print("The message is too long. Please try again.")
+        print("Your message is too long. Try again.")
         return
+    # import datetime
+    # x = datetime.datetime.now()
+    # https: // www.w3schools.com / python / python_datetime.asp
     date_time = datetime.datetime.now()
-    traveler_name = input("Enter your name (leave empty for 'anonymous'): ")
-    if not traveler_name:
-        traveler_name = "anonymous"
+    passenger_name = input("Write your name (or leave it empty for 'anonymous'): ")
+    if not passenger_name:
+        passenger_name = "anonymous"
     station = random.choice(stations)
-    return message, date_time, traveler_name, station
+    return message, date_time, passenger_name, station
 
 def save_message(message):
     with open('messages.csv', mode='a', newline='') as messages_file:
