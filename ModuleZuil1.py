97c3ad63eab492de576b69d80b3a3ad218fbad09
Morid Aziz <ahmad.morid.aziz@hva.nl>
2023-09-22 23:17:39 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	main.py

---
+++ ModuleZuil1.py
@@ -0,0 +1,18 @@
+import csv
+import datetime
+import random
+stations = ["Amsterdam CS", "Utrecht CS", "Leiden CS"]
+def type_your_message_():
+    message = input("Write a message of maximum 140 characters in: ")
+    if len(message) > 140:
+        print("Your message is too long. Try again.")
+        return
+    date_time = datetime.datetime.now()
+    name = input("Tyoe your name (Or leave it 'anonymous'): ")
+    if not name:
+        name = "anonymous"
+    station = random.choice(stations)
+    return message, date_time, name, station
+def save_messages(message):
+    with open('messages.csv', mode='a', newline='') as message_file:
+
