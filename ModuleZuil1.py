308f9094febd221528b4c8e0ce23d85985df02ce
Morid Aziz <ahmad.morid.aziz@hva.nl>
2023-09-22 23:35:01 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	main.py

--- ModuleZuil1.py
+++ ModuleZuil1.py
@@ -1,20 +1,22 @@
 import csv
 import datetime
 import random
-stations = ["Amsterdam CS", "Utrecht CS", "Leiden CS"]
-def type_your_message_():
-    message = input("Write a message of maximum 140 characters in: ")
+
+stations = ["Station A", "Station B", "Station C"]
+def enter_message():
+    message = input("Enter your message (up to 140 characters): ")
     if len(message) > 140:
-        print("Your message is too long. Try again.")
+        print("The message is too long. Please try again.")
         return
     date_time = datetime.datetime.now()
-    name = input("Type your name (Or leave it 'anonymous'): ")
-    if not name:
-        name = "anonymous"
+    traveler_name = input("Enter your name (leave empty for 'anonymous'): ")
+    if not traveler_name:
+        traveler_name = "anonymous"
     station = random.choice(stations)
-    return message, date_time, name, station
-def save_messages(message):
-    with open('messages.csv', mode='a', newline='') as message_file:
-        message_Writer = csv.writer(message_file)
-        message_Writer.writerow(message)
+    return message, date_time, traveler_name, station
+
+def save_message(message):
+    with open('messages.csv', mode='a', newline='') as messages_file:
+        messages_writer = csv.writer(messages_file)
+        messages_writer.writerow(message)
 
